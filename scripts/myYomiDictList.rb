5d87a9803e7e19fa2ceb3b968fc2375502d8f1a6
和田勇(Isamu WADA) <wada136@mac.com>
2023-03-10 12:07:48 +0900

    fix yomi.dict に置き換えたため

--- scripts/myYomiDictList.rb
+++ scripts/myYomiDictList.rb
@@ -6,9 +6,7 @@ require_relative 'myGlobalValue'
 require_relative 'myYomi'
 
 if __FILE__ == $0 then
-  File.open( $YOMI_DB_CACHE_FILE, "w")  do | f |
-    $YOMI_DB_CACHE.sort.each do | k, v |
-      f.puts "#{k} => #{v}" unless k =~ /textrm|んんん/
-    end
+  $YOMI_DB_CACHE.sort.each do | k, v |
+    puts "#{k} => #{v}" unless k =~ /textrm|んんん/
   end
 end
